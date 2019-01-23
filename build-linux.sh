a57ca0a77c1c54d1a2b18bf674e0830092ae509f
Ussama Naal <usa.naal@gmail.com>
2019-01-23 13:15:08 -0600

    Create scripts to easily generate and build the workspace

---
+++ build-linux.sh
@@ -0,0 +1,2 @@
+cmake -S . -B"build/x64-Debug" -DCMAKE_BUILD_TYPE=Debug
+cmake -S . -B"build/x64-Release" -DCMAKE_BUILD_TYPE=Release
---
+++ build-mac.sh
@@ -0,0 +1,2 @@
+cmake -S . -B"build/x64-Debug" -DCMAKE_BUILD_TYPE=Debug -G Xcode
+cmake -S . -B"build/x64-Release" -DCMAKE_BUILD_TYPE=Release -G Xcode
